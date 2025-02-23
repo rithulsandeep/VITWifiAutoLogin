d3281e3c9bb16989e7026d5814667250a6847b52
rithulsandeep <imrithulsandeep@gmail.com>
2025-02-23 08:13:55 +0530

    Fixed auto-closing tab issue after login

--- background.js
+++ background.js
@@ -1,19 +1,29 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.storage.sync.get(["username", "password"], (data) => {
-        if (data.username && data.password) {
-            // If credentials exist, log in
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: autoLogin,
-                args: [data.username, data.password]
-            });
-        } else {
-            // Ask for credentials
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: getUserCredentials
-            });
-        }
+chrome.action.onClicked.addListener(() => {
+    const loginURL = "http://phc.prontonetworks.com/cgi-bin/authlogin?URI=http://www.gstatic.com/generate_204";
+
+    // Open a new tab with the login page
+    chrome.tabs.create({ url: loginURL }, (tab) => {
+        chrome.storage.sync.get(["username", "password"], (data) => {
+            if (data.username && data.password) {
+                // If credentials exist, log in
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    function: autoLogin,
+                    args: [data.username, data.password]
+                });
+                
+                // Close the tab after a delay (ensuring login completes)
+                setTimeout(() => {
+                    chrome.tabs.remove(tab.id);
+                }, 3000);
+            } else {
+                // Ask for credentials
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    function: getUserCredentials
+                });
+            }
+        });
     });
 });
 
@@ -21,7 +31,7 @@ function autoLogin(username, password) {
     document.querySelector('input[name="userId"]').value = username;
     document.querySelector('input[name="password"]').value = password;
 
-    // Automatically submit the login form
+    // Submit the form
     document.querySelector('input[name="Submit22"]').click();
 }
 
@@ -30,7 +40,6 @@ function getUserCredentials() {
     let password = prompt("Enter your WiFi password:");
 
     if (username && password) {
-        // Store credentials in Chrome Storage
         chrome.storage.sync.set({ username, password }, () => {
             alert("Credentials saved! Click the extension again to log in.");
         });
